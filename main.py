e808ff3a1309cd942d4d9a5eb2f56bbf8fed3eb0
ogomii <62138668+ogomii@users.noreply.github.com>
2022-07-20 22:26:15 +0200

    init

---
+++ main.py
@@ -0,0 +1,34 @@
+import pygame as pg
+
+
+if __name__ == "__main__":
+    pg.init()
+    size = width, height = 1280, 720
+
+    screen = pg.display.set_mode(size)
+
+    background = pg.Surface(size)
+    background = background.convert()
+    background.fill((170, 238, 187))
+
+    if pg.font:
+        font = pg.font.Font(None, 64)
+        text = font.render("Snek", True, (10, 10, 10))
+        textpos = text.get_rect(centerx=background.get_width() / 2, y=10)
+        background.blit(text, textpos)
+    
+    screen.blit(background, (0, 0))
+    pg.display.flip()
+
+    clock = pg.time.Clock()
+
+    keepGameRunning = True
+    while keepGameRunning:
+        #max 60 fps
+        clock.tick(60)
+
+        for event in pg.event.get():
+            if event.type == pg.QUIT or event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE:
+                keepGameRunning = False
+    
+    pg.quit()
